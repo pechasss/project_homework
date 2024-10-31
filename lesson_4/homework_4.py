304968262cbfd2979a7886e2fcc86814510592c9
pechasss <egorpeacha66@gmail.com>
2024-10-31 17:22:54 +0100

    homework

---
+++ lesson_4/homework_4.py
@@ -0,0 +1,28 @@
+# lst = [0, 1, 0, 2, 3]
+# number= 0
+#
+# for i in range(len(lst)):
+#     if lst[i] != 0:
+#         lst[number] = lst[i]
+#         number += 1
+#
+# while number < len(lst):
+#     lst[number] = 0
+#     number += 1
+#
+# print(lst)
+
+
+
+# ex2
+
+
+
+# lst = [0, 1, 2, 4, 8]
+# #
+# if lst:
+#     sumOfNumbers= sum(lst[i] for i in range(0, len(lst), 2))
+#     result = sumOfNumbers * lst[-1]
+# else:
+#     result = 0
+# print(result)
