27a87724e1ecc6e9b996f21fb440635b26816d6d
pechasss <egorpeacha66@gmail.com>
2024-11-05 16:27:05 +0100

    homework

--- lesson_5/homework_5.py
+++ lesson_5/homework_5.py
@@ -44,3 +44,32 @@
 # print(create_hashtag(user_word))
 
 
+
+# ex5.1
+# import string
+# import keyword
+#
+#
+# def valid_name(name):
+#
+#     if name in keyword.kwlist:
+#         return False
+#
+#     if name[0].isdigit():
+#         return False
+#
+#     if any(char.isupper() for char in name):
+#         return False
+#
+#     invalid_chars = set(string.punctuation) - {"_"}
+#     if any(char in invalid_chars for char in name):
+#         return False
+#
+#     if name.count("_") > 1:
+#         return False
+#
+#     return True
+# user_name= input("Enter name:")
+# print(valid_name(user_name))
+
+
